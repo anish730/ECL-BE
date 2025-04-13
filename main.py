ccb9a1604977af8e10753e83899c8ca61636f315
anish730 <anish.pdl09@gmail.com>
2025-04-13 16:39:42 +0545

    test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from server import views
 from utils.encoder import DobatoEncoder
 from utils.extensions import db
@@ -10,7 +11,6 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
-
     with app.app_context():
         db.create_all()
 
@@ -18,14 +18,18 @@ def create_app():
 
 
 app = create_app()
+CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+                             "headers": ["Content-Type", "Authorization", "ngrok-skip-browser-warning"]}})
 app.json = DobatoEncoder(app)
 app.add_url_rule('/api/v1/users', view_func=views.UserListApi.as_view('ecl-users-api'))
 app.add_url_rule('/api/v1/business-industry', view_func=views.BusinessIndustryApi.as_view('business-industry-api'))
 app.add_url_rule('/api/v1/risk-decisions', view_func=views.RiskDecisionApi.as_view('risk-decisions'))
 app.add_url_rule('/api/v1/cib-data', view_func=views.FetchCIBData.as_view('cib-data'))
 app.add_url_rule('/api/v1/loans', view_func=views.CustomerLoanApi.as_view('customer-loans'))
+app.add_url_rule('/api/v1/payments', view_func=views.LoanPaymentsApi.as_view('loan-payments'))
 app.add_url_rule('/api/v1/ecl-calculation', view_func=views.ECLCalculationApi.as_view('ecl-calculations'))
+app.add_url_rule('/api/v1/lending-types', view_func=views.LendingTypeAPI.as_view('lending-types-api'))
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
